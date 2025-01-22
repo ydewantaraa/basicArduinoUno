d4429f928f47c6a919d8be342da4e278d398ad2e
Yudha Dewantara <110522695+ydewantaraa@users.noreply.github.com>
2025-01-22 17:52:08 +0700

    Add files via upload

---
+++ 09_microServo.ino
@@ -0,0 +1,24 @@
+#include <Servo.h>
+
+int pos = 0;
+
+Servo servo_9;
+
+void setup()
+{
+    servo_9.attach(9, 500, 2500);
+}
+
+void loop()
+{
+    for (pos = 0; pos <= 180; pos += 1)
+    {
+        servo_9.write(pos);
+        delay(15);
+    }
+    for (pos = 180; pos >= 0; pos -= 1)
+    {
+        servo_9.write(pos);
+        delay(15);
+    }
+}
