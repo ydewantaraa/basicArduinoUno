d020247e3e4d5aa931fc01909dd412e3dc759f29
Yudha Dewantara <110522695+ydewantaraa@users.noreply.github.com>
2025-03-11 14:32:50 +0700

    Update #05_buzzerBelwithButton.ino

--- #05_buzzerBelwithButton.ino
+++ #05_buzzerBelwithButton.ino
@@ -1,18 +1,19 @@
-int tombol = 0;
+int tombol;
+int aktif = 0;
 int Buzzer = 8;
 
 void setup()
 {
-  pinMode(7, INPUT);
+  pinMode(7, INPUT_PULLUP);
   pinMode(9, OUTPUT);
 }
 
 void loop()
 {	
-	tombol = digitalRead(7);
-  if(tombol == HIGH){
+  tombol = digitalRead(7);
+  if(tombol == aktif){
     digitalWrite(9, HIGH);
-    tone (Buzzer,500,2);
+    tone (Buzzer,500,5);
     delay(2);
   }
   else{
